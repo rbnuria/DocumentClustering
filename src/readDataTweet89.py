33adaebe5ca2a0546c8bb80d67bf20065f8993f6
rbnuria <rbnuria6@gmail.com>
2019-05-28 12:13:52 +0200

    mierda

--- src/readDataTweet89.py
+++ src/readDataTweet89.py
@@ -45,7 +45,12 @@ class ReaderTweet89(Reader):
 				self._data.append(preprocessing.tokenize(sentence))
 
 			if self._concatenate:
-				self._data = preprocessing.padding_truncate(self._data, 20)
+				max_length = 0
+				for sentence in self._data:
+					if len(sentence) > max_length:
+						max_length = len(sentence)
+
+				self._data = preprocessing.padding_truncate(self._data, max_length)
 
 			self._vectors = np.array(preprocessing.word2embeddings(self._data, self._embedding, self._vocabulary, self._concatenate))
 		else:
