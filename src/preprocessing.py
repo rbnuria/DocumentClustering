2589c8f64a52bcd9163ec5b282b3b1c9a6d38ea2
rbnuria <rbnuria6@gmail.com>
2019-05-28 11:56:13 +0200

    mierda

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -74,16 +74,13 @@ def word2embeddings(data, embedding, vocab, concatenate):
 		sentence_embedding = []
 		
 		if concatenate:
-
 			for word in sentence:
 				if word == 0:
-					sentence_embedding.extend(embeddings_matrix[vocabulary['PADDING']])
+					sentence_embedding.extend(embeddings_matrix[vocabulary['PADDING']].astype(np.float))
 				elif word in vocabulary:
-					sentence_embedding.extend(embeddings_matrix[vocabulary[word]])
+					sentence_embedding.extend(embeddings_matrix[vocabulary[word]].astype(np.float))
 				else:
-					sentence_embedding.extend(embeddings_matrix[vocabulary['UNKOWN']])
-
-			data_embeddings.append(sentence_embedding)
+					sentence_embedding.extend(embeddings_matrix[vocabulary['UNKOWN']].astype(np.float))
 
 			data_embeddings.append(sentence_embedding)
 
