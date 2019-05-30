c3e7748457eeb90f315a02e30e32993fb1e09d0d
rbnuria <rbnuria6@gmail.com>
2019-05-30 15:32:59 +0200

    frequent words

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -47,7 +47,7 @@ def get_top_n_words(corpus, n=None):
     vec = CountVectorizer().fit(corpus)
     bag_of_words = vec.transform(corpus)
     sum_words = bag_of_words.sum(axis=0) 
-    words_freq = [(word, sum_words[0, idx]) for word, idx in     vec.vocabulary_.items()]
+    words_freq = [word for word, idx in     vec.vocabulary_.items()]
     words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
     return words_freq[:n]
 
@@ -55,7 +55,7 @@ def apply_stemmer_stopword(data):
 	ps = PorterStemmer()
 
 	data = get_top_n_words(data, 2000)
-	print(data[][1])
+	print(data[1])
 
 	stemmed_data = []
 
