5eeea0a7db72e84f26ec28f5d165ca608bebc31d
rbnuria <rbnuria6@gmail.com>
2019-05-30 13:26:52 +0200

    Change main

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
 	#Leemos los embeddings 
 	#COMENTAR CUANDO HAGAMOS TF-IDF
-	embeddings, vocabulary = preprocessing.read_embeddings("../crawl-300d-2M.vec")
+	#embeddings, vocabulary = preprocessing.read_embeddings("../crawl-300d-2M.vec")
 
 	#Leemos los datos
 
@@ -20,27 +20,22 @@ if __name__ == "__main__":
 	#data = ReaderTweet89("../data/Tweet", "embeddings", embeddings, vocabulary, True)
 
 	#Embeddings-media
-	data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
+	#data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
 	#data = ReaderReutersR52("../data/r52-train-stemmed.txt", "../data/r52-test-stemmed.txt", "embeddings", embeddings, vocabulary)
 
 	#tf-idf
-	data_2 = ReaderTweet89("../data/20ng.txt", "tfidf")
+	data = ReaderTweet89("../data/20ng.txt", "tfidf")
 	
 	tweets = data.get_vectors()
-	tweets_2 = data_2.get_vectors()
 	labels_true = data.get_clusters()
 
 	print("Etiquetas reales: ", labels_true)
 
 	#Aplicamos kmeans
-	kmeans = KMeans(n_clusters = 20, random_state = 1234567, n_init = 25, max_iter = 500).fit(tweets)
-	kmeans_tfidf = KMeans(n_clusters = 20, random_state = 1234567, n_init = 25, max_iter = 500).fit(tweets_2)
+	kmeans = KMeans(n_clusters = 20, random_state = 1234567, n_init = 10, max_iter = 100).fit(tweets)
 
 	print("Etiquetas predichas: ", kmeans.labels_)
 	
 	nmi = NMI(labels_true, kmeans.labels_)
 	print(nmi)
-
-	nmi_tfidf = NMI(labels_true, kmeans_tfidf.labels_)
-	print(nmi_tfidf)
 
