7ae38cca46637f1f2e3da24512ea4402e5221dcc
rbnuria <rbnuria6@gmail.com>
2019-05-30 16:54:22 +0200

    frequent words

--- src/main.py
+++ src/main.py
@@ -7,12 +7,14 @@ from readDataTweet89 import *
 from readDataReuters import *
 from metrics import homogenity, completeness, NMI
 import preprocessing
+import csv
+
 
 if __name__ == "__main__":
 
 	#Leemos los embeddings 
 	#COMENTAR CUANDO HAGAMOS TF-IDF
-	#embeddings, vocabulary = preprocessing.read_embeddings("../crawl-300d-2M.vec")
+	embeddings, vocabulary = preprocessing.read_embeddings("../crawl-300d-2M.vec")
 
 	#Leemos los datos
 
@@ -20,17 +22,24 @@ if __name__ == "__main__":
 	#data = ReaderTweet89("../data/Tweet", "embeddings", embeddings, vocabulary, True)
 
 	#Embeddings-media
-	#data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
+	data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
 	#data = ReaderReutersR52("../data/r52-train-stemmed.txt", "../data/r52-test-stemmed.txt", "embeddings", embeddings, vocabulary)
 
 	#tf-idf
 	data = ReaderTweet89("../data/20ng.txt", "tfidf")
 	
 	tweets = data.get_vectors()
-
-	print(tweets[1])
-
 	labels_true = data.get_clusters()
+
+	f = open("tweets.csv", "wb")
+	writer = csv.writer(f)
+
+	writer.write(tweets)
+
+	f1 = open("labels.csv", "wb")
+	writer = csv.writer(f1)
+
+	writer.write(labels_true)
 
 	print("Etiquetas reales: ", labels_true)
 
