080dcad34cd02bdf1607b199c1b1747ed81b1853
rbnuria <rbnuria6@gmail.com>
2019-05-30 18:23:12 +0200

    frequent words

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
 	#Leemos los embeddings 
 	#COMENTAR CUANDO HAGAMOS TF-IDF
-	embeddings, vocabulary = pre.read_embeddings("../crawl-300d-2M.vec")
+	#embeddings, vocabulary = pre.read_embeddings("../crawl-300d-2M.vec")
 
 	#Leemos los datos
 
@@ -23,33 +23,33 @@ if __name__ == "__main__":
 	#data = ReaderTweet89("../data/Tweet", "embeddings", embeddings, vocabulary, True)
 
 	#Embeddings-media
-	data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
+	#data = ReaderTweet89("../data/20ng.txt", "embeddings", embeddings, vocabulary)
 	#data = ReaderReutersR52("../data/r52-train-stemmed.txt", "../data/r52-test-stemmed.txt", "embeddings", embeddings, vocabulary)
 
 	#tf-idf
-	#data = ReaderTweet89("../data/20ng.txt", "tfidf")
+	data = ReaderTweet89("../data/20ng.txt", "tfidf")
 	
 	tweets = data.get_vectors()
 	labels_true = data.get_clusters()
 
 
 
-	#f = open("tweets.csv", "wb")
-	#writer = csv.writer(f)
+	f = open("tweets.csv", "wb")
+	writer = csv.writer(f)
 
-	#writer.write(tweets)
+	writer.write(tweets)
 
-	#f1 = open("labels.csv", "wb")
-	#writer = csv.writer(f1)
+	f1 = open("labels.csv", "wb")
+	writer = csv.writer(f1)
 
-	#writer.write(labels_true)
+	writer.write(labels_true)
 
 	#tweets = preprocessing.normalize(tweets)
 
 	print("Etiquetas reales: ", labels_true)
 
 	#Aplicamos kmeans
-	kmeans = KMeans(n_clusters = 20, random_state = 1234567, n_init = 10, max_iter = 100).fit(tweets)
+	kmeans = KMeans(n_clusters = 20, random_state = 1234567, n_init = 2, max_iter = 100).fit(tweets)
 
 	#agglomerative = AgglomerativeClustering(n_clusters = 20, affinity = "cosine", linkage = "complete").fit(tweets)
 
